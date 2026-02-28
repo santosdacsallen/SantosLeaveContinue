90a8411b7192bed6acc8819cda67c196c4882212
CLARENCE <clarencetilaon.69@gmail.com>
2026-02-28 11:16:42 +0800

    Update Program.cs

--- SantosGit/Program.cs
+++ SantosGit/Program.cs
@@ -4,38 +4,63 @@
     {
         static void Main(string[] args)
         {
-
             Console.WriteLine("Enter Name of the Employee: ");
-            var EmpName = Console.ReadLine();
-            Console.WriteLine("Choose number reason of leave: (1) sick 10 days, (2) vacation 10 days, (3) Paternity 7 days , (4) Maternity 107 days");
-            int reason = Convert.ToInt16(Console.ReadLine());
+            string empName = Console.ReadLine();
+
+            Console.WriteLine("Choose number reason of leave:");
+            Console.WriteLine("(1) Sick Leave - 10 days");
+            Console.WriteLine("(2) Vacation Leave - 10 days");
+            Console.WriteLine("(3) Paternity Leave - 7 days");
+            Console.WriteLine("(4) Maternity Leave - 107 days");
+
+            int reason = Convert.ToInt32(Console.ReadLine());
+
             Console.WriteLine("Enter number of Days to leave: ");
-            int days = Convert.ToInt16(Console.ReadLine());
+            int days = Convert.ToInt32(Console.ReadLine());
 
-
-            int sick = 10 - days;
-            int vacation = 10 - days;
-            int paternity = 7 - days;
-            int maternity = 107 - days;
+            int maxDays = 0;
+            string leaveType = "";
 
             if (reason == 1)
             {
-                Console.WriteLine("You have " + sick + " remaining of sick days");
+                maxDays = 10;
+                leaveType = "Sick Leave";
             }
             else if (reason == 2)
             {
-                Console.WriteLine("You have " + vacation + " remaining of sick days");
+                maxDays = 10;
+                leaveType = "Vacation Leave";
             }
             else if (reason == 3)
             {
-                Console.WriteLine("You have " + paternity + " remaining of sick days");
+                maxDays = 7;
+                leaveType = "Paternity Leave";
             }
             else if (reason == 4)
             {
-                Console.WriteLine("You have " + maternity + " remaining of sick days");
+                maxDays = 107;
+                leaveType = "Maternity Leave";
+            }
+            else
+            {
+                Console.WriteLine("Invalid choice.");
             }
 
+            if (days > maxDays)
+            {
+                Console.WriteLine("Leave request denied. Exceeded allowed days.");
+            }
+            else
+            {
+                int remaining = maxDays - days;
 
+                Console.WriteLine("\n--- Leave Summary ---");
+                Console.WriteLine("Employee Name: " + empName);
+                Console.WriteLine("Leave Type: " + leaveType);
+                Console.WriteLine("Days Filed: " + days);
+                Console.WriteLine("Remaining Leave Balance: " + remaining);
+                Console.WriteLine("Leave request approved.");
+            }
         }
     }
 }
